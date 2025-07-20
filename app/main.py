f575a6f3e2d3ed42c4347544d789cde8a653e52a
yeonjy73 <yeon703@naver.com>
2025-07-20 18:25:36 +0900

    fix: fraud-analysis api endpoint

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.routers import fraud_analysis
 app = FastAPI()
 
 # router 등록
-app.include_router(fraud_analysis.router, prefix="/api/fraud_analysis", tags=["fraud_analysis"])
+app.include_router(fraud_analysis.router, prefix="/api/fraud-analysis", tags=["fraud-analysis"])
 
 @app.get("/api/hello")
 async def hello():
