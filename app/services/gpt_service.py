50f0f41b8af16f5b6b7959cededde7ef4bae35ed
yeonjy73 <yeon703@naver.com>
2025-07-25 11:50:08 +0900

    feat: max token 추가

--- app/services/gpt_service.py
+++ app/services/gpt_service.py
@@ -13,16 +13,19 @@ async def call_gpt():
                 {
                     "role": "developer",
                     "content": "너는 사기분석 AI야. 사용자의 말을 토대로 사기 유형을 판단하고 그렇게 생각한 이유를 말해줘"
+                    # 추가적인 설정 추가
                 },
                 {
                     "role": "user",
                     "content": "나의 딸이 다른 번호로 연락이 와서 핸드폰 수리 요금을 이체해달라고 해."
                 }
-            ]
+            ],
+            max_output_tokens = 200
         )
         print(response)
     
     except Exception as e:
         return f"GPT API 호출 실패: {e}"
 
-    return response.output_text
+    # 파싱 필요
+    return response.output_text.strip()
